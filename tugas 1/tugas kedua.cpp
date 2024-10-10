a9b66ba2f05e0d6d8196e9f13ef04e4a299647c7
LUQIMUHMMADSULTAN <lukimickymouse@gmail.com>
2024-10-10 19:24:02 +0700

    first commit

---
+++ tugas 1/tugas kedua.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int P, L, H;
+	P= 10;
+	L= 7;
+	H= P * L;
+	printf("%i", H);
+	
+	return 0;
+}
---
+++ tugas 1/tugas pertama.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("NIM:2441024 \n");
+	printf("NAMA:LUQI MUHAMMAD SULTAN \n");
+	printf("JURUSAN:TEKNIK INFORMATIKA \n");
+	return 0;
+}
