e86e250a3a69d4f0adb89704adff00989ed13d7c
LUQIMUHMMADSULTAN <lukimickymouse@gmail.com>
2024-11-14 23:07:36 +0700

    first commit

---
+++ tugas 5/tugas 1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int hitung_lama_bekerja(int jam_masuk, int jam_pulang) {
+    int lama_bekerja;
+    if (jam_pulang >= jam_masuk) {
+        lama_bekerja = jam_pulang - jam_masuk;
+    }
+    else {
+        lama_bekerja = (12 - jam_masuk) + jam_pulang;
+    }
+
+    return lama_bekerja;
+}
+
+int main() {
+    int jam_masuk, jam_pulang, lama_bekerja;
+
+    printf("Masukkan jam masuk (1-12): ");
+    scanf("%d", &jam_masuk);
+    printf("Masukkan jam pulang (1-12): ");
+    scanf("%d", &jam_pulang);
+    lama_bekerja = hitung_lama_bekerja(jam_masuk, jam_pulang);
+    printf("Lama bekerja: %d jam\n", lama_bekerja);
+
+    return 0;
+}
